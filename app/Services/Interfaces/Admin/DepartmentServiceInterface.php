faf187768e256fc9d65f329d0713251fff353167
Ppw99 <pyae.phyowin2003@icloud.com>
2025-02-19 16:26:55 +0630

    Departments (crud api) (#2)
    * department-api-half-completed
    
    * departments-CRUD-api-fully-completed
    
    * department CRUD code flow changes
    
    ---------
    
    Co-authored-by: Pyae Phyo Win <mba131707@MBA131707s-MacBook-Air.local>
    Co-authored-by: Pyae Phyo Win <pyaephyowin706@gmail.com>

---
+++ app/Services/Interfaces/Admin/DepartmentServiceInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Interfaces\Admin;
+
+use App\Models\Department;
+use Illuminate\Http\Request;
+
+interface DepartmentServiceInterface
+{
+    public function getDepartments(Request $request);
+    public function getDepartmentById($id);
+    public function create(array $data);
+    public function update(Department $department, array $data);
+}
