6000e2f7f4277ad0bb1edb19712decfbb559462b
Min Thit Htoo <133160196+Jamy62@users.noreply.github.com>
2025-02-26 23:48:23 +0630

    factories-added

---
+++ database/factories/IdeaFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Closure;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Idea>
+ */
+class IdeaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'content' => $this->faker->paragraphs(3, true),
+            'closure_id' => fn () => Closure::inRandomOrder()->first()->id ?? Closure::factory()->create()->id,
+            'user_id' => fn () => User::inRandomOrder()->first()->id ?? User::factory()->create()->id,
+        ];
+    }
+}
