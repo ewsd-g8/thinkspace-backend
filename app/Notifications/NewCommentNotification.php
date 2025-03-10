8d0943bd36b28675650647146fee9479149e8a89
EthanHank <pyae.phyowin2003@icloud.com>
2025-03-10 13:11:21 +0630

    first comment for noti

---
+++ app/Notifications/NewCommentNotification.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use App\Models\Comment;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class NewCommentNotification extends Notification
+{
+    use Queueable;
+
+
+    protected $comment;
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(Comment $comment)
+    {
+        $this->comment = $comment;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('New Comment on Your Idea')
+            ->line('Someone has commented on your idea: ' . $this->comment->idea->title)
+            ->line('Comment: ' . $this->comment->content)
+            ->action('View Comment', url('/ideas/' . $this->comment->idea_id))
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
